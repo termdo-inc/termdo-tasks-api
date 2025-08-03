76adc47c517d7437231d8b2f3f2e44a5f1744a27
Emrecan Karaçayır <developer@karacayir.com>
2025-08-04 01:21:04 +0300

    Changed: routes

--- source/termdo_tasks_api/core/tasks/tasks_builder.py
+++ source/termdo_tasks_api/core/tasks/tasks_builder.py
@@ -8,18 +8,14 @@ tasks_router = APIRouter()
 _manager = TasksManager()
 
 
-@tasks_router.get(
-    "/accounts/{account_id}/tasks", status_code=status.HTTP_200_OK
-)
+@tasks_router.get("{account_id}", status_code=status.HTTP_200_OK)
 async def get_tasks(
     account_id: int = Path(gt=0),
 ) -> list[TasksResponse]:
     return await _manager.get_tasks(account_id)
 
 
-@tasks_router.post(
-    "/accounts/{account_id}/tasks", status_code=status.HTTP_201_CREATED
-)
+@tasks_router.post("{account_id}", status_code=status.HTTP_201_CREATED)
 async def post_task(
     account_id: int = Path(gt=0),
     task: TasksRequest = Body(),
@@ -27,9 +23,7 @@ async def post_task(
     return await _manager.post_task(account_id, task)
 
 
-@tasks_router.get(
-    "/accounts/{account_id}/tasks/{task_id}", status_code=status.HTTP_200_OK
-)
+@tasks_router.get("{account_id}/{task_id}", status_code=status.HTTP_200_OK)
 async def get_task(
     account_id: int = Path(gt=0),
     task_id: int = Path(gt=0),
@@ -37,9 +31,7 @@ async def get_task(
     return await _manager.get_task(account_id, task_id)
 
 
-@tasks_router.put(
-    "/accounts/{account_id}/tasks/{task_id}", status_code=status.HTTP_200_OK
-)
+@tasks_router.put("{account_id}/{task_id}", status_code=status.HTTP_200_OK)
 async def put_task(
     account_id: int = Path(gt=0),
     task_id: int = Path(gt=0),
@@ -49,8 +41,7 @@ async def put_task(
 
 
 @tasks_router.delete(
-    "/accounts/{account_id}/tasks/{task_id}",
-    status_code=status.HTTP_204_NO_CONTENT,
+    "{account_id}/{task_id}", status_code=status.HTTP_204_NO_CONTENT
 )
 async def delete_task(
     account_id: int = Path(gt=0),
